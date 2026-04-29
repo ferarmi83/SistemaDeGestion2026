11ca71512a10c57c6e2b81f284b4b3417c0bff46
Fernando Aramayo Michel <ferarmi83@gmail.com>
2026-04-29 16:52:56 -0400

    Agregar archivos de proyecto.

---
+++ SistemaDeGestion2026/FRMPrincipal.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace SistemaDeGestion2026
+{
+    partial class FRMPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FRMPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FRMPrincipal";
+            this.Opacity = 0D;
+            this.Text = "Tienda de Ropa";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FRMPrincipal_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ SistemaDeGestion2026/FRMPrincipal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaDeGestion2026
+{
+    public partial class FRMPrincipal : Form
+    {
+        public FRMPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void FRMPrincipal_Load(object sender, EventArgs e)
+        {
+            FRMSplash banner = new FRMSplash();
+            banner.ShowDialog();
+            this.Opacity = 1;
+        }
+    }
+}
