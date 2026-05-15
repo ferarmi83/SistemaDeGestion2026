a13bbe2c306b65aa8aef9559f94f33bba4957823
Fernando Aramayo Michel <ferarmi83@gmail.com>
2026-05-15 16:45:38 -0400

    15/05/2026

---
+++ SistemaDeGestion2026/FRMPersona_Registrar.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SistemaDeGestion2026
+{
+    partial class FRMPersona_Registrar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GPPanelPrincipal = new DevComponents.DotNetBar.Controls.GroupPanel();
+            this.SuspendLayout();
+            // 
+            // GPPanelPrincipal
+            // 
+            this.GPPanelPrincipal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(239)))), ((int)(((byte)(242)))));
+            this.GPPanelPrincipal.CanvasColor = System.Drawing.SystemColors.Control;
+            this.GPPanelPrincipal.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.GPPanelPrincipal.DisabledBackColor = System.Drawing.Color.Empty;
+            this.GPPanelPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GPPanelPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.GPPanelPrincipal.Name = "GPPanelPrincipal";
+            this.GPPanelPrincipal.Size = new System.Drawing.Size(412, 356);
+            // 
+            // 
+            // 
+            this.GPPanelPrincipal.Style.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.GPPanelPrincipal.Style.BackColorGradientAngle = 90;
+            this.GPPanelPrincipal.Style.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.GPPanelPrincipal.Style.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.GPPanelPrincipal.Style.BorderBottomWidth = 1;
+            this.GPPanelPrincipal.Style.BorderColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.GPPanelPrincipal.Style.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.GPPanelPrincipal.Style.BorderLeftWidth = 1;
+            this.GPPanelPrincipal.Style.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.GPPanelPrincipal.Style.BorderRightWidth = 1;
+            this.GPPanelPrincipal.Style.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.GPPanelPrincipal.Style.BorderTopWidth = 1;
+            this.GPPanelPrincipal.Style.CornerDiameter = 4;
+            this.GPPanelPrincipal.Style.CornerType = DevComponents.DotNetBar.eCornerType.Rounded;
+            this.GPPanelPrincipal.Style.TextAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Center;
+            this.GPPanelPrincipal.Style.TextColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+            this.GPPanelPrincipal.Style.TextLineAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Near;
+            // 
+            // 
+            // 
+            this.GPPanelPrincipal.StyleMouseDown.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // 
+            // 
+            this.GPPanelPrincipal.StyleMouseOver.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.GPPanelPrincipal.TabIndex = 0;
+            this.GPPanelPrincipal.Text = "Persona";
+            // 
+            // FRMPersona_Registrar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 356);
+            this.Controls.Add(this.GPPanelPrincipal);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FRMPersona_Registrar";
+            this.Text = "FRMPersona_Registrar";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.Controls.GroupPanel GPPanelPrincipal;
+    }
+}
